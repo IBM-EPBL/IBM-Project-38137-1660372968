70f865db94d855ad86175b875598d6f98a6e2a30
Rajarajeswari.J <74948755+RAJIE11@users.noreply.github.com>
2022-11-19 21:27:02 +0530

    Add files via upload

---
+++ Project development phase/Sprint 4/Sprint 4/credentials.py
@@ -0,0 +1,10 @@
+COS_ENDPOINT="https://s3.jp-tok.cloud-object-storage.appdomain.cloud"
+COS_API_KEY_ID="v7JvGIH7V0f3UZL6FvgPU0zs-rgpZKAvj2W3DoClvRRA"
+COS_INSTANCE_CRN="crn:v1:bluemix:public:cloud-object-storage:global:a/3d4040a37c90488084c76adf29cf1ad7:d1356827-f772-4c07-b31e-68e17dfcb914::"
+
+
+DB2_DATABASE_NAME="bludb"
+DB2_HOST_NAME="0c77d6f2-5da9-48a9-81f8-86b520b87518.bs2io90l08kqb1od8lcg.databases.appdomain.cloud"
+DB2_PORT="31198"
+DB2_UID="drk70799"
+DB2_PWD="wtqbpnIQ2OzZ1649"
